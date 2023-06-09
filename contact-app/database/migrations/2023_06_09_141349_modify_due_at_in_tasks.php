e4592b959b59c9fba78de891f2f457e6676500b6
rpencis <roberts.pencis@di.lv>
2023-06-09 17:47:47 +0300

    section 7 - working with database part 1

---
+++ contact-app/database/migrations/2023_06_09_141349_modify_due_at_in_tasks.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     * 
+     * rename requires -- composer require doctrine/dbal
+     */
+    public function up(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->renameColumn('due_at','due_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->renameColumn('due_date','due_at');
+        });
+    }
+};

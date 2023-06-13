2a286d3278c72761f1989c9f32456de17659d873
Roberts <15999503+RPencis@users.noreply.github.com>
2023-06-13 23:00:33 +0300

    s-10
    section 10 - Model Factories

--- contact-app/database/seeders/DatabaseSeeder.php
+++ contact-app/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Company;
+use App\Models\Contact;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,9 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            CompanySeeder::class,
-            ContactSeeder::class
-        ]);
+        //order is important
+        Company::factory(10)->hasContacts(10)->create();
+
     }
 }
